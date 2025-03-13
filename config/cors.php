21c7fc4e5dc3c92f44b0dcf3fbf7f585d637af72
bruceoyugi <oyugibruce2017@gmail.com>
2025-03-13 04:58:24 -0400

    add: vercel to the cors

--- config/cors.php
+++ config/cors.php
@@ -4,8 +4,9 @@ return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:8080'),
-        'https://animal-haven-manager.app.genez.io'
+        env('FRONTEND_URL', 'http://localhost:8080'), // Local development URL
+        'https://animal-haven-manager.app.genez.io', // Existing production URL
+        'https://animal-haven-manager.vercel.app' // New Vercel URL
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
